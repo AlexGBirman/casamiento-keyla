eb3f03235f583df92b3f273067dacd318ecba5de
Alex Birman <AlexGBirman>
2023-11-19 01:20:58 -0300

    carrusel completado

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -23,10 +23,10 @@
     <div class="contenedor-fecha">
       <div class="col-md-78">
         <div class="countdown-container">
+          <div class="mb-4">
+            <img src="../assets/fecha.jpg" alt="" class="img-fluid" width=80%>
+          </div>
           <div class="countdown" v-if="timeRemaining > 0">
-            <div class="mb-4">
-              <img src="../assets/fecha.jpg" alt="" class="img-fluid" width=80%>
-            </div>
             <div class="mb-4">
               <h3 style="font-size: 1.3rem; margin-top: 55px;">FALTAN</h3>
             </div>
@@ -62,65 +62,65 @@
       </div>
     </div> <!-- hecho -->
 
-    <div class="ubicacion container-fluid text-center">
-      <img src="../assets/" alt="">
-      <p style="font-family: Italianno; font-size: 50px;">Quinta Querandí</p>
-      <p>Azcuénaga 1318, Gral. Rodríguez, Provincia de Buenos Aires</p>
-      <button @click="verUbicacion()" class="btn btn-light btn-lg" style="font-family: Poppins;">VER UBICACIÓN</button>
-    </div>
-
-    <div class="container">
-      <div id="carouselExampleIndicators" class="carousel slide">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-            aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-            aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-            aria-label="Slide 3"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
-            aria-label="Slide 4"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4"
-            aria-label="Slide 5"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5"
-            aria-label="Slide 6"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6"
-            aria-label="Slide 7"></button>
-        </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="../assets/carru1.jpg" class="d-block w-100 imagen-carrusel" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="../assets/carru2.jpg" class="d-block w-100 imagen-carrusel" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="../assets/carru3.jpg" class="d-block w-100 imagen-carrusel" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="../assets/carru4.jpg" class="d-block w-100 imagen-carrusel" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="../assets/carru5.jpg" class="d-block w-100 imagen-carrusel" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="../assets/carru6.jpg" class="d-block w-100 imagen-carrusel" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="../assets/carru7.jpg" class="d-block w-100 imagen-carrusel" alt="...">
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
+    <div class="ubicacion container-fluid h-auto">
+      <img src="../assets/ubicacion.png" alt="" height="100px" id="ubicacion-png" style="margin-top: 20px;">
+      <div class="container-fluid text-center">
+        <p style="font-family: Italianno; font-size: 3rem; margin-bottom: 0; margin-top: 10px;">Quinta Querandí</p>
+        <p>Azcuénaga 1318, Gral. Rodríguez, Provincia de Buenos Aires</p>
+        <button @click="verUbicacion()" class="btn btn-light btn-lg" style="font-family: Poppins;">VER UBICACIÓN</button>
       </div>
+    </div> <!-- hecho -->
+
+    <div id="carouselExample" class="carousel slide">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="row">
+            <div class="col-4">
+              <img src="../assets/carru1.jpg" class="d-block w-100 imagen-carrusel" alt="...">
+            </div>
+            <div class="col-4">
+              <img src="../assets/carru2.jpg" class="d-block w-100 imagen-carrusel" alt="...">
+            </div>
+            <div class="col-4">
+              <img src="../assets/carru3.jpg" class="d-block w-100 imagen-carrusel" alt="...">
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="row">
+            <div class="col-4">
+              <img src="../assets/carru1.jpg" class="d-block w-100 imagen-carrusel" alt="...">
+            </div>
+            <div class="col-4">
+              <img src="../assets/carru2.jpg" class="d-block w-100 imagen-carrusel" alt="...">
+            </div>
+            <div class="col-4">
+              <img src="../assets/carru3.jpg" class="d-block w-100 imagen-carrusel" alt="...">
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="row">
+            <div class="col-4">
+              <img src="../assets/carru1.jpg" class="d-block w-100 imagen-carrusel" alt="...">
+            </div>
+            <div class="col-4">
+              <img src="../assets/carru2.jpg" class="d-block w-100 imagen-carrusel" alt="...">
+            </div>
+            <div class="col-4">
+              <img src="../assets/carru3.jpg" class="d-block w-100 imagen-carrusel" alt="...">
+            </div>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
 
     <div class="asistencia container-fluid text-center">
@@ -130,38 +130,6 @@
         Por favor, confirmar asistencia antes del día 03/01/2024</p>
       <button @click="confirmarAsistencia()" class="btn btn-light btn-lg" style="font-family: Poppins;">CONFIRMAR
         ASISTENCIA</button>
-    </div>
-
-    <!-- <div id="carrusel-container" :style="{ transform: 'translateX(' + translateValue + '%)' }">
-      <img v-for="(imagen, index) in imagenes" :key="index" :src="imagen" :alt="'Imagen ' + (index + 1)" class="imagen-carrusel">
-    </div> -->
-
-    <div class="container mt-5 mb-5">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="card mi-card">
-            <img src="../assets/percha.png" alt="" id="icon" class="card-img-top mx-auto">
-            <div class="card-body text-center">
-              <h5 class="card-title">Dress code</h5>
-              <p class="card-text">Elegante y elegante sport</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="card mi-card">
-            <img src="../assets/checkmark.png" alt="" id="icon" class="card-img-top mx-auto">
-            <div class="card-body text-center">
-              <h5 class="card-title">Info general</h5>
-              <ul>
-                <li>El lugar tiene estacionamiento con capacidad para todos los autos</li>
-                <li>Chequeá tu invitación unos días antes de la fiesta para encontrar tu ubicación de mesa y algunos
-                  detalles más</li>
-                <li>Te pedimos que seas puntual ya que la ceremonia comenzará 19:30hs</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
 
   </section>
@@ -174,19 +142,12 @@ export default {
   props: [],
   mounted() {
     setInterval(this.updateCountdown, 1000)
-    setInterval(this.mostrarSiguiente, 3000)
     this.updateCountdown()
-    for (let i = 1; i <= 7; i++) {
-      this.imagenes.push(`../assets/carru${i}.jpg`);
-      console.log(this.imagenes)
-    }
   },
   data() {
     return {
       targetDate: new Date("2024-01-20T19:00:00").getTime(),
       timeRemaining: 0,
-      imagenes: [],
-      currentIndex: 0,
     }
   },
   methods: {
@@ -218,11 +179,7 @@ export default {
       return -this.currentIndex * 33.33;
     },
   }
-
 }
-
-
-
 </script>
 
 <style scoped lang="css">
@@ -238,7 +195,7 @@ export default {
 
 .ubicacion {
   background: #E4EBD9;
-  height: 300px;
+  height: 310px;
   text-align: center;
   align-items: center;
 }
@@ -265,33 +222,8 @@ export default {
   src: url('../fonts/italianno-regular-webfont.woff') format('truetype');
 }
 
-.rounded-circle {
-  background: #E4EBD9;
-}
-
-/* #carrusel-container {
-  display: flex;
-  transition: transform 0.5s ease-in-out;
-} */
-
-.imagen-carrusel {
-  width: 33.33%;
-  flex-shrink: 0;
-}
-
 .src-components-index {
   background: #FEFBFB;
-}
-
-#icon {
-  width: 100px;
-}
-
-.mi-card {
-  border-radius: 15px;
-  /* Ajusta el radio del borde según tus preferencias */
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  /* Ajusta la sombra según tus preferencias */
 }
 
 .navbar-brand {
@@ -335,14 +267,6 @@ export default {
   align-items: center;
 }
 
-.texto-frase {
-  height: 200px;
-  /* Ajusta la altura del contenedor según tus preferencias */
-  align-items: center;
-  justify-content: center;
-  display: flex;
-}
-
 .contenedor-fecha {
   background-image: url('../assets/fondo-countdown.jpg');
   padding: 20px;
@@ -356,11 +280,10 @@ export default {
   justify-content: center;
 }
 
-.contenedor2 img {
-  max-width: 100%;
+.imagen-carrusel {
+  flex-shrink: 0;
+  display: inline-block;
+  padding: 30px 15px;
 }
 
-.centrar-contenedor {
-  width: 80%
-}
 </style>
